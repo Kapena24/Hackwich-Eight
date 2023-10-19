bfdc649ac6e279f257630e04707c0213c7345036
Kapena Kaaihue <142773521+Kapena24@users.noreply.github.com>
2023-10-19 12:57:24 -1000

    Completed part 1-4

--- Hackwich Eight/ViewController.swift
+++ Hackwich Eight/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myLabel.text = "My Segmented Control"
     }
 
+    @IBAction func segmentedControlPressed(_ sender: Any) {
+        switch
+segmentedControl.selectedSegmentIndex 
+        {
+        case 0:
+            myLabel.text = "First Segment has been selected"
+        case 1:
+            myLabel.text = "Second Segment has been selected"
+        default: break
+        }
 
+    }
+    
 }
 
